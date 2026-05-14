a9ce61e2aea3dba4acf0c3d7097559d3dbf7a0f7
Andre <andresgideao@gmail.com>
2026-05-14 14:42:13 +0100

    Estrutura de pastas e estilizacao no react native

---
+++ src/app/index/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import { styles } from './styled'
+
+export default function App() {
+  return (
+    <View>
+      <Text
+        style={styles.title}>
+        Hello, World ANDRE!
+      </Text>
+    </View>
+  );
+}
+
